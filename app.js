1033ae721cda0c0825824825ddbb405022a12da7
MichaelThomasMPT <michaelthomas.mpt@gmail.com>
2020-03-29 21:55:55 +0800

    Missed a comma in app.js from the previous merge. Also forgot to update Steven's config.

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function get_widgets(SETTINGS) {
         {label: 'Name', value: SETTINGS.algolia.index_name},
         {label: 'BGG Rank', value: SETTINGS.algolia.index_name + '_rank_ascending'},
         {label: 'Number of ratings', value: SETTINGS.algolia.index_name + '_numrated_descending'},
-        {label: 'Number of owners', value: SETTINGS.algolia.index_name + '_numowned_descending'}
+        {label: 'Number of owners', value: SETTINGS.algolia.index_name + '_numowned_descending'},
         {label: 'Last Modified', value: SETTINGS.algolia.index_name + '_lastmodified_descending'}
       ]
     }),
